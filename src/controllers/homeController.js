b757ae87d3f892e62314aab08cd48aadf173f431
Khai Do <khaidooo192@gmail.com>
2024-06-20 01:57:10 +0700

    4 commit

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,6 @@
 
 import db from '../models/index';
+import CRUDService from '../services/CRUDService';
 let getHomePage = async (req,res) =>{
     try {
         let data = await db.User.findAll();
@@ -10,14 +11,33 @@ let getHomePage = async (req,res) =>{
     } catch (error) {
         console.log(error);
     }
-   
 }
-
-
 let getAboutPage =(req,res) =>{
     return res.render('test/about.ejs')
 }
+
+let getCRUD =(req,res) =>{
+    return res.render('crud.ejs')
+}
+let postCRUD = async(req, res) =>{
+   let message = await CRUDService.createNewUser(req.body);
+   console.log(message);
+    return res.send ('Post crud from sever')
+}
+let displayGetCRUD =async(req,res) => {
+   let data = await CRUDService.getAllUser();
+   console.log('-------------------------------');
+   console.log(data);
+   console.log('-------------------------------');
+    return res.render ('displayCRUD.ejs',{
+        dataTable :data
+    })
+    
+}
 module.exports ={
     getHomePage : getHomePage,
-    getAboutPage : getAboutPage
+    getAboutPage : getAboutPage,
+    getCRUD : getCRUD,
+    postCRUD : postCRUD,
+    displayGetCRUD: displayGetCRUD,
 }
