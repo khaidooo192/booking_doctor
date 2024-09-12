0c5bc1961353bcb37dd11a2321b0c1d06bc12ae5
Khai Do <khaidooo192@gmail.com>
2024-09-13 02:52:43 +0700

    api crud backend first

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -25,7 +25,55 @@ let handleLogin = async (req,res) =>{
 
 }
 
+let handleGetAllUsers = async(req,res) =>{
+    let id = req.query.id; //All / Id 
+    if(!id){
+        return res.status(200).json({
+            errCode : 1,
+            errMessage : 'Missing require parameter',
+            users: []
+        })
+    }
+    let users = await userService.getAllUsers(id);
+    console.log(users);
+    return res.status(200).json({
+        errCode : 0,
+        errMessage : 'ok',
+        users
+    })
+    
+}
+
+let handleCreateNewUser = async(req,res) =>{
+   let message = await userService.createNewUser(req.body);
+   console.log(message);
+   return res.status(200).json(message)
+}
+
+let handleDeleteUser = async(req,res) =>{
+    if(!req.body.id){
+        return res.status(200).json({
+            errCode : 1,
+            errMessage : 'Missing require parameter'
+        })
+    }
+    let message = await userService.deleteUser(req.body.id);
+    console.log(message);
+    return res.status(200).json( message )
+}
+
+let handleEditUser = async(req,res) => {
+    let data =req.body
+    let message = await userService.editUser(data);
+    return res.status(200).json(message)
+}
+
+
 module.exports = {
     handleLogin : handleLogin,
+    handleGetAllUsers : handleGetAllUsers,
+    handleCreateNewUser : handleCreateNewUser,
+    handleEditUser : handleEditUser,
+    handleDeleteUser : handleDeleteUser
 
 }
--- src/services/userService.js
+++ src/services/userService.js
@@ -2,6 +2,21 @@ import { where } from "sequelize"
 import bcrypt from'bcryptjs';
 import db from "../models/index"
 import { raw } from "body-parser";
+
+const salt = bcrypt.genSaltSync(10);
+
+let hashUserPassword =(password) => {
+    return new Promise( async(resolve, reject) => {
+        try {
+            let hashPassword = await bcrypt.hashSync(password, salt);    
+            resolve(hashPassword)
+        } catch (e) {
+            reject(e)
+        }
+        
+    } )
+ }
+
 let handleUserLogin =(email, password) => {
     return new Promise (async(resolve,reject) => {
         try {
@@ -49,8 +64,6 @@ let handleUserLogin =(email, password) => {
 
 }
 
-
-
 let checkUserEmail =(userEmail) => {
     return new Promise(async(resolve, reject)=>{
         try {
@@ -67,6 +80,135 @@ let checkUserEmail =(userEmail) => {
         }
     })
 }
+
+let getAllUsers =(userId) => {
+    return new Promise( async(resolve,reject) => {
+        try {
+            let users = '';
+            if(userId === 'ALL'){
+                users = await db.User.findAll({
+                    attributes : {
+                        exclude :['password']
+                    }
+                })
+            }if(userId && userId !== 'ALL'){
+                users = await db.User.findOne({
+                    where :{id : userId},
+                    attributes : {
+                        exclude :['password']
+                    }
+                })
+            }
+            resolve(users)
+
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+
+let createNewUser = (data) =>{
+    return new Promise( async(resolve,reject) =>{
+        try {
+            //check email
+            let check = await checkUserEmail(data.email);
+            if(check=== true){
+                resolve({
+                    errCode : 1,
+                    errMessage: `Your's Email is already in used, plz try another email`
+                })
+            }else{
+                let hashPasswordFromBcrypt = await hashUserPassword(data.password);
+                await db.User.create({
+                email: data.email,
+                password: hashPasswordFromBcrypt,
+                firstName: data.firstName,
+                lastName: data.lastName,
+                address: data.address,
+                phonenumber:data.phonenumber,
+                gender: data.gender === '1' ? true : false,
+                roleId:data.roleId
+            })
+            resolve({
+                errCode : 0,
+                errMessage: 'OK'   
+
+            })
+            }
+  
+        } catch (e) {
+            reject(e)
+        }
+    })
+} 
+
+let deleteUser = (userId) => {
+    return new Promise(async(resolve, reject) => {
+        let foundUser = await db.User.findOne({
+            where : {id : userId}
+        })
+        if(!foundUser){
+            resolve({
+                errCode: 2,
+                errMessage: `the user isn't exist`
+            })
+        }
+        if(foundUser){
+            await db.User.destroy({
+                where : {id : userId}
+            })
+        }
+        
+        resolve({
+            errCode:0,
+            errMessage: `Delete user succeed!`
+        })
+    })
+}
+
+let editUser = (data) => {
+    return new Promise (async(resolve,reject) =>{
+        if(!data.id){
+            resolve({
+                errCode: 2,
+                errMessage: 'Missing require parameter'
+            })
+        }
+        try {
+            let user =  await db.User.findOne({
+                where : {id :data.id},
+                raw :false
+            })
+            if(user){
+                user.firstName= data.firstName;
+                user.lastName= data.lastName;
+                user.address= data.address;
+                await user.save()
+                // await db.User.save({
+                //     firstName: data.firstName,
+                //     lastName: data.lastName, 
+                //     address: data.address
+                // })
+                resolve({
+                    errCode: 0,
+                    errMessage : 'Update user succeed !'
+                });
+            }else{
+                resolve({
+                    errCode: 1,
+                    errMessage: 'User not found'
+                })
+            }
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
 module.exports={
-    handleUserLogin: handleUserLogin
+    handleUserLogin: handleUserLogin,
+    getAllUsers: getAllUsers,
+    createNewUser : createNewUser,
+    deleteUser : deleteUser,
+    editUser :editUser
 }
